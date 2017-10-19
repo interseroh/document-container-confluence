187d3cf24f81d73f9355455ebfdd6cb6fcfa7a01
lofidewanto <lofidewanto@gmail.com>
2017-10-19 16:34:12 +0200

    update some stuffs

---
+++ src/main/java/com/lofidewanto/demo/server/domain/AttachmentImpl.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.lofidewanto.demo.server.domain;
+
+public class AttachmentImpl implements Attachment {
+
+	private String id;
+	private String title;
+	private String downloadLink;
+	private String mediaType;
+	private String fileSize;
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getTitle() {
+		return title;
+	}
+
+	@Override
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String getDownloadLink() {
+		return downloadLink;
+	}
+
+	@Override
+	public void setDownloadLink(String downloadLink) {
+		this.downloadLink = downloadLink;
+	}
+
+	@Override
+	public String getMediaType() {
+		return mediaType;
+	}
+
+	@Override
+	public void setMediaType(String mediaType) {
+		this.mediaType = mediaType;
+	}
+
+	@Override
+	public String getFileSize() {
+		return fileSize;
+	}
+
+	@Override
+	public void setFileSize(String fileSize) {
+		this.fileSize = fileSize;
+	}
+}
