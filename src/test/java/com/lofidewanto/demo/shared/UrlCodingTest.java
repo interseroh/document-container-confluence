1d7039fd5835a4218e611bf88de067476d621c35
lofidewanto <lofidewanto@gmail.com>
2017-11-21 18:21:46 +0100

    update test

--- src/test/java/com/lofidewanto/demo/shared/UrlCodingTest.java
+++ src/test/java/com/lofidewanto/demo/shared/UrlCodingTest.java
@@ -1,20 +1,24 @@
 package com.lofidewanto.demo.shared;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
-public class TestUrlCoding {
+public class UrlCodingTest {
 
     private static final String URL_STRING = "http://www.test.com?test1=1&test2=2";
-    private static final String ENCODED_URL_STRING = "http://www.text.com__-__test1=1--_--test2=2";
+    private static final String ENCODED_URL_STRING = "http://www.test.com__-__test1=1--_--test2=2";
 
-    void testEncoding() {
+    @Test
+    public void testEncoding() {
         UrlCoding urlCoding = new UrlCoding(URL_STRING);
         String result = urlCoding.encode();
 
         assertEquals(result, ENCODED_URL_STRING);
     }
 
-    void testDecoding() {
+    @Test
+    public void testDecoding() {
         UrlCoding urlCoding = new UrlCoding();
         String result = urlCoding.decode(ENCODED_URL_STRING);
 
